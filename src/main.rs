764ae06e2bec02a9f76207ea906f729d1e70aa1b
softprops <d.tangren@gmail.com>
2019-03-10 16:03:04 -0400

    update deps + fmt

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,14 @@ use rusoto_events::{CloudWatchEvents, CloudWatchEventsClient, ListTargetsByRuleR
 
 use chrono::Duration;
 
-fn get_ecs_task_def_arn(events: &CloudWatchEvents, rule_name: &str) -> Option<String> {
+fn get_ecs_task_def_arn(
+    events: &CloudWatchEvents,
+    rule_name: &str,
+) -> Option<String> {
     events
-        .list_targets_by_rule(&ListTargetsByRuleRequest {
+        .list_targets_by_rule(ListTargetsByRuleRequest {
             rule: rule_name.into(),
-            ..Default::default()
+            ..ListTargetsByRuleRequest::default()
         })
         .sync()
         .map(|response| {
@@ -32,12 +35,17 @@ fn get_ecs_task_def_arn(events: &CloudWatchEvents, rule_name: &str) -> Option<St
         .unwrap_or_default()
 }
 
+/// get the timestamp of the last time a given rule triggered an event
 /// https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cwe-metricscollected.html
-fn get_last_trigger(metrics: &CloudWatch, rule_name: &str, since: Duration) -> Option<String> {
+fn get_last_trigger(
+    metrics: &CloudWatch,
+    rule_name: &str,
+    since: Duration,
+) -> Option<String> {
     let now = Utc::now();
     let start = now - since;
     metrics
-        .get_metric_statistics(&GetMetricStatisticsInput {
+        .get_metric_statistics(GetMetricStatisticsInput {
             dimensions: Some(vec![Dimension {
                 name: "RuleName".into(),
                 value: rule_name.into(),
@@ -48,7 +56,7 @@ fn get_last_trigger(metrics: &CloudWatch, rule_name: &str, since: Duration) -> O
             period: Duration::days(1).num_seconds(),
             start_time: start.to_rfc3339(),
             statistics: Some(vec!["Sum".into()]),
-            ..Default::default()
+            ..GetMetricStatisticsInput::default()
         })
         .sync()
         .map(|response| {
@@ -64,9 +72,9 @@ fn get_last_trigger(metrics: &CloudWatch, rule_name: &str, since: Duration) -> O
 }
 
 fn main() {
-    let events = CloudWatchEventsClient::simple(Default::default());
-    let metrics = CloudWatchClient::simple(Default::default());
-    let ecs = EcsClient::simple(Default::default());
+    let events = CloudWatchEventsClient::new(Default::default());
+    let metrics = CloudWatchClient::new(Default::default());
+    let ecs = EcsClient::new(Default::default());
 
     match (env::args().nth(1), env::args().nth(2)) {
         (Some(rule), Some(cluster)) => {
@@ -75,22 +83,25 @@ fn main() {
                 get_last_trigger(&metrics, rule.as_str(), Duration::weeks(1))
             );
             let task_def = get_ecs_task_def_arn(&events, rule.as_str());
-            for arn in task_def {
+            if let Some(arn) = task_def {
                 println!("inspecting tasks for {}", arn);
-                let tasks = ecs.list_tasks(&ListTasksRequest {
-                    cluster: Some(cluster.clone()),
-                    desired_status: Some("STOPPED".into()),
-                    started_by: Some(format!("events-rule/{}", rule)[..36].into()),
-                    ..Default::default()
-                }).sync()
+                let tasks = ecs
+                    .list_tasks(ListTasksRequest {
+                        cluster: Some(cluster.clone()),
+                        desired_status: Some("STOPPED".into()),
+                        started_by: Some(format!("events-rule/{}", rule)[..36].into()),
+                        ..ListTasksRequest::default()
+                    })
+                    .sync()
                     .ok()
                     .and_then(|response| {
-                        ecs.describe_tasks(&DescribeTasksRequest {
+                        ecs.describe_tasks(DescribeTasksRequest {
                             cluster: Some(cluster.clone()),
                             tasks: response.task_arns.unwrap_or_default(),
-                        }).sync()
-                            .map(|response| response.tasks.unwrap_or_default())
-                            .ok()
+                        })
+                        .sync()
+                        .map(|response| response.tasks.unwrap_or_default())
+                        .ok()
                     })
                     .unwrap_or_default();
 
@@ -105,7 +116,7 @@ fn main() {
         }
         _ => {
             eprintln!(
-                "usage: {} <rule_name> <cluster_name>",
+                "usage: {} <rule_name> <ecs_cluster_name>",
                 env::args().next().unwrap_or_default()
             );
         }
